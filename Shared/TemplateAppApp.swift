d5cbef5fd6e16a590261b34b8899030deea0decd
drei <andrei@bitcoin.com>
2022-03-26 21:45:13 +0900

    Initial Commit

---
+++ Shared/TemplateAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemplateAppApp.swift
+//  Shared
+//
+//  Created by Andrei Terentiev on 3/26/22.
+//
+
+import SwiftUI
+
+@main
+struct TemplateAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
